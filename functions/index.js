29fa914113b4400494b69794ccf735985a761b3c
kislaya1996 <kislayamishra96@gmail.com>
2020-02-11 18:22:47 +0900

    Deployed initial build on firebase

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,32 +6,3 @@ const admin = require('firebase-admin');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-admin.initializeApp();
-
-exports.onMessageCreate = functions.firestore
-    .document('messages/{userId}')
-    .onCreate((snap, context) => {
-        // Check if limit is reached
-        const newValue = snap.data();
-        console.log(newValue);
-        const ref = admin.firestore().collection('messages').orderBy('timestamp');
-
-        ref.onSnapshot(snapshot => {
-            i = 0;
-            size = snapshot.size;
-            sizeToDelete = size - 10;
-            console.log('Messages Count: ' + size);
-            
-            snapshot.forEach((doc) => {
-                if (i < sizeToDelete) {
-                    doc.ref.delete().then(()=>{
-                        console.log("Document successfully deleted");
-                    }).catch((error) => {
-                        console.log("Error removing document: ", error);
-                    });
-                }
-                i++;  
-            });
-        });
-    });
